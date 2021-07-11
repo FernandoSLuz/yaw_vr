d014dd1e4c2963a751f9c7b1572f5f9ccb63ca5a
FernandoSLuz <fernandosilvadaluz@gmail.com>
2021-07-11 15:01:58 -0300

    updates on mechanics

---
+++ Assets/_Scripts/Paraglider_Controller.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lighthouse{
+	public class Paraglider_Controller : MonoBehaviour
+	{
+		public Wing_Controller left_wing_controller;
+		public Wing_Controller right_wing_controller;
+	}
+}
+
