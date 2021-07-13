e0592ec2515307d3e531d54ca42fa80d4323002a
FernandoSLuz <fernandosilvadaluz@gmail.com>
2021-07-13 14:26:04 -0300

    updates on ring game

---
+++ Assets/_Scripts/Ring_Instance.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lighthouse{
+    public class Ring_Instance : MonoBehaviour
+    {
+        public MeshRenderer meshRenderer;
+        public RingState ringState;
+		private void Start()
+		{
+            StartCoroutine(LoopLookAt());
+		}
+		private void OnTriggerEnter(Collider other)
+		{
+			if(other.tag == "Player"){
+                Ring_Manager.instance.RingCaught(this);
+            }
+		}
+        public IEnumerator LoopLookAt(){
+            var target = GameObject.FindGameObjectWithTag("Player").transform;
+            while(true){
+
+                Vector3 targetPostition = new Vector3(target.position.x,
+                                        this.transform.parent.position.y,
+                                        target.position.z);
+                this.transform.parent.LookAt(targetPostition);
+
+                yield return null;
+            }
+        }
+
+		[System.Serializable]
+        public enum RingState{ 
+            qeued,
+            active,
+            used
+        }
+    }
+}
+
