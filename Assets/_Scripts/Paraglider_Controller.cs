88bda4576206bb7bd5be2079b3e91b0c3ca25268
FernandoSLuz <fernandosilvadaluz@gmail.com>
2021-07-13 11:48:04 -0300

    updates

--- Assets/_Scripts/Paraglider_Controller.cs
+++ Assets/_Scripts/Paraglider_Controller.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 using BNG;
 using YawVR;
 using System.Net;
-namespace Lighthouse{
+namespace Lighthouse
+{
 	public class Paraglider_Controller : MonoBehaviour
 	{
 		public Wing_Controller leftWingHolder;
@@ -18,7 +19,12 @@ namespace Lighthouse{
 
 		Transform t;
 		float YRotation = 0;
+		public float velocity = 2;
+		public float rotVelocity = 2;
+		public float smooth = 2;
+		public float rotationSmooth = 1;
 		public YawController yawController; // reference to YawController
+		public AnimationCurve curve;
 
 		private IEnumerator Start()
 		{
@@ -27,17 +33,21 @@ namespace Lighthouse{
 			yield return new WaitForSeconds(1.0f);
 			yawController = YawController.Instance();
 			yawController.ConnectToDevice(new YawDevice(IPAddress.Parse("192.168.0.12"), 50020, 50010, "001", "DEBUG", DeviceStatus.Available),
-					() =>{Debug.Log("Sucesso");}, (error) => { Debug.Log(error); }
+					() => { Debug.Log("Sucesso"); }, (error) => { Debug.Log(error); }
 					);
 		}
 		private void Update()
 		{
-			Debug.Log(yawController.State);
+			//Debug.Log(yawController.State);
 		}
 
-		public IEnumerator MoveRoutine(){
+		public IEnumerator MoveRoutine()
+		{
 			yield return new WaitForSeconds(5.0f);
-			while(true){
+			float currentWingPosition = 0;
+			float currentRotation = 0;
+			while (true)
+			{
 				rightWingPos = rightWingHolder.holderPosition;
 				leftWingPos = leftWingHolder.holderPosition;
 
@@ -48,15 +58,18 @@ namespace Lighthouse{
 				float targetWingMovement = -(leftWingPos.magnitude / 10) + rightWingPos.magnitude / 10;
 
 				targetWingMovement = Mathf.Clamp(targetWingMovement, -8, 8);
-
+				currentWingPosition = Mathf.SmoothDamp(currentWingPosition, targetWingMovement, ref velocity, Time.deltaTime, smooth);
 				//If both left and right wings are 0,0,0, it should go front
 				//If right wing are 0,y!=0,0 and left wing is 0,0,0 it should go right
 				//If left wing are 0,y!=0,0 and right wing is 0,0,0 it should go left
 
 				//wing movement must be smoothDamped
+				float targetRotation = -currentWingPosition * ZRotationLimit;
+				currentRotation = Mathf.SmoothDamp(currentRotation, -currentWingPosition * ZRotationLimit, ref rotVelocity,
+					curve.Evaluate(Mathf.Abs(currentWingPosition / 8) * rotationSmooth));
 
-				YRotation += targetWingMovement * Time.deltaTime * yRotationVelocity;
-				t.rotation = Quaternion.Euler(0, YRotation, -targetWingMovement * ZRotationLimit);
+				YRotation += currentWingPosition * Time.deltaTime * yRotationVelocity;
+				t.rotation = Quaternion.Euler(0, YRotation, currentRotation);
 				t.position += (t.forward * Time.deltaTime * forwardVelocity);
 				//yawController.TrackerObject.SetRotation(transform.localEulerAngles);
 				yield return null;
